7a2df4d9e52e6e43d2a51a56b6dadc47015023bf
mdkirin <mdkirin@gmail.com>
2025-03-04 01:21:41 +0900

    레이어 1번 썸클러스터 수정 및 EXTRAKEY_ENABLE 추가

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/mdkirin/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/mdkirin/keymap.c
@@ -116,7 +116,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
         KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , MT(MOD_RSFT,KC_J)     , KC_K     , KC_L     , KC_ENT  ,
         KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , LT(1,KC_COMM)  , KC_DOT   , LT(3,KC_SLSH)  ,
-        KC_LCTL  , KC_LGUI  , KC_LALT  , LT(1,KC_LNG1) , LT(2,KC_SPC), KC_LSFT, LT(2,KC_BSPC), LT(1,KC_SPC), _______, _______, _______, MO(4)
+        KC_LCTL  , KC_LGUI  , KC_LALT  , LT(1,KC_ENT) , LT(2,KC_SPC), LT(3,KC_LNG1), LT(2,KC_BSPC), LT(1,KC_SPC), _______, _______, _______, MO(4)
     ),
 
     [1] = LAYOUT_universal(
